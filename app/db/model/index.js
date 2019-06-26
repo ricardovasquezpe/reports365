160669ffefe1e4d925bfeb230e2259c432657f80
Ricardo Vasquez <ricardo.vasquez.pe@gmail.com>
2019-06-26 18:47:34 -0500

    Adding english

--- app/db/model/index.js
+++ app/db/model/index.js
@@ -5,4 +5,12 @@ module.exports = function(app, jwt){
     res.render('index', { title: 'Express' });
   });
 
+  app.get('/en', function(req, res, next) {
+    res.render('indexen', { title: 'Express' });
+  });
+
+  app.get('/es', function(req, res, next) {
+    res.render('indexes', { title: 'Express' });
+  });
+
 }
